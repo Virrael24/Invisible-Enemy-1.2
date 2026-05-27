d353dade8c6c83e967abafc3b82a90181720f19b
Virrael24 <t.zykova.msk@gmail.com>
2026-05-27 19:47:00 +0300

    please help

---
+++ Invis/Assets/Systems/My dialogue/Scripts/DialogueNode.cs
@@ -0,0 +1,10 @@
+/*using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogueNode", menuName = "Dialogue/Node")]
+public class DialogueNode : ScriptableObject
+{
+    public DialogueLine[] lines;
+    public DialogueNode nextNode; // Проверьте, что это поле ВНЕ класса DialogueLine
+    public int minRelationshipRequired = 0;
+}
+*/
